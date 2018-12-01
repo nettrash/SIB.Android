47e426169c9ecdf0d2558ed20fd68eb929889e70
NETTRASH <nettrash@nettrash.ru>
2018-12-01 13:24:59 +0300

    1.15

--- app/src/main/java/ru/nettrash/sibcoin/sibAPI.java
+++ app/src/main/java/ru/nettrash/sibcoin/sibAPI.java
@@ -104,7 +104,7 @@ public final class sibAPI {
     }
 
     public sibAPI() {
-        urlAPIRoot = "https://api.sib.moe/wallet/sib.svc";
+        urlAPIRoot = "https://service.biocoin.pro/wallet/sib/sib.svc";
         userAPI = "SIB";
         passwordAPI = "E0FB115E-80D8-4F4E-9701-E655AF9E84EB";
     }
