defc98a255ce0a57817a0456a960e1f8ba4a8b56
NETTRASH <nettrash@nettrash.ru>
2019-03-21 09:20:17 +0300

    readme

--- app/src/main/java/ru/nettrash/sibcoin/database/IntArrayToString.java
+++
@@ -1,8 +0,0 @@
-package ru.nettrash.sibcoin.database;
-
-/**
- * Created by nettrash on 21.01.2018.
- */
-
-public final class IntArrayToString {
-}
--- app/src/main/java/ru/nettrash/sibliteandroid/Database.java
+++
@@ -1,8 +0,0 @@
-package ru.nettrash.sibliteandroid;
-
-/**
- * Created by nettrash on 21.01.2018.
- */
-
-public class Database {
-}
